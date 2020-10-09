6e64e44d1c2445b84be1a1b0d863707d394358e7
ayan poddar <ayanpoddar9@gmail.com>
2020-10-10 00:16:57 +0530

    Add my files

---
+++ ImageProccesserBeasic/ImageProccesserBeasic/Model/ImageDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace ImageProccesserBeasic.Model
+{
+    class ImageDataModel
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public List<List<Color>> Data { get; set; }
+    }
+}
