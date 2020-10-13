96f1bf3e05e28c94dd1d4f15fd7191826103ec09
ayan poddar <ayanpoddar9@gmail.com>
2020-10-14 02:17:15 +0530

    a working example

--- ImageProccesserBeasic/ImageProccesserBeasic/Program.cs
+++ ImageProccesserBeasic/ImageProccesserBeasic/Program.cs
@@ -1,6 +1,7 @@
 ﻿using ImageProccesserBeasic.Model;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Threading.Tasks;
@@ -12,33 +13,60 @@ namespace ImageProccesserBeasic
         static async Task Main(string[] args)
         {
             // change the path to a valid image
-            var data = await GetImageDataAsync(@"C:\Users\ayanp\Desktop\IMG_20200925_215107.jpg");
+            var data = await GetImageDataAsync(@"C:\Users\ayanp\Desktop\image1.jpg");
 
-            // create a blank image
-            var img = new Bitmap(data.Width, data.Height);
+            // resize the photo
+            var sampleSize = 50;
 
-            // copy the image data in to the blank image 
-            for (int i = 0; i < data.Height - 1; i++)
-            {
-                for (int j = 0; j < data.Width - 1; j++)
-                {
-                    img.SetPixel(j, i, data.Data[i][j]);
-                }
-            }
+            var w = Math.Ceiling((decimal)data.Width / sampleSize);
+            var h = Math.Ceiling((decimal)data.Height / sampleSize);
+
+            int width = (int)(w * sampleSize);
+            int height = (int)(h * sampleSize);
+
+            // create a blank resized image
+            var img = new Bitmap(width, height);
+
 
             // make a squere in that place
-            for (int i = 1000; i < 3000; i++)
+            for (int i = 0; i < data.Height; i += sampleSize)
             {
-                for (int j = 2000; j < 3000; j++)
+                for (int j = 0; j < data.Width; j += sampleSize)
                 {
-                    img.SetPixel(j, i, Color.White);
+                    //img.SetPixel(j, i, data.Data[i][j]);
+                    square(j, i, sampleSize, sampleSize, data.Data[i][j], img);
                 }
             }
-            // change the output path too
+
+            // change the output path toos
             img.Save(@"C:\Users\ayanp\Desktop\IMG_20200925_215108.jpg", ImageFormat.Jpeg);
         }
 
-        // get all pixels data about the image and width and height too
+        /// <summary>
+        /// draw a squre in a picture
+        /// </summary>
+        /// <param name="x">x position of the image</param>
+        /// <param name="y">y posation of the image</param>
+        /// <param name="width">width of the square</param>
+        /// <param name="height">height of the square</param>
+        /// <param name="color">fill color of the square</param>
+        /// <param name="host">image where the square will be drawn</param>
+        static void square(int x, int y, int width, int height, Color color, Bitmap host)
+        {
+            for (int i = x; i < x + height; i++)
+            {
+                for (int j = y; j < y + width; j++)
+                {
+                    host.SetPixel(i, j, color);
+                }
+            }
+        }
+
+        /// <summary>
+        /// get all pixels data about the image and width and height too
+        /// </summary>
+        /// <param name="Path"> where the image is stored</param>
+        /// <returns></returns>
         static async Task<ImageDataModel> GetImageDataAsync(string Path)
         {
             return await Task.Run(() =>
