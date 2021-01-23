30bc1ccf3530d6f3bf0c540d049e6cd7ded083d9
Ayan Poddar <ayanpoddar0@gmail.com>
2021-01-23 17:48:04 +0530

    add C# code

---
+++ AccessingPixels/AccessPixelsInCSharp/AccessPixelsInCSharp/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace AccessPixelsInCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.GetData = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(785, 68);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnOpenFile.Location = new System.Drawing.Point(759, 0);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(26, 68);
+            this.btnOpenFile.TabIndex = 1;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // GetData
+            // 
+            this.GetData.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.GetData.Location = new System.Drawing.Point(0, 0);
+            this.GetData.Name = "GetData";
+            this.GetData.Size = new System.Drawing.Size(89, 68);
+            this.GetData.TabIndex = 2;
+            this.GetData.Text = "Get data";
+            this.GetData.UseVisualStyleBackColor = true;
+            this.GetData.Click += new System.EventHandler(this.GetData_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(874, 445);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.WordWrap = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(874, 68);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.btnOpenFile);
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(785, 68);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.GetData);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(785, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(89, 68);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 68);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(874, 445);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 513);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button GetData;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ AccessingPixels/AccessPixelsInCSharp/AccessPixelsInCSharp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccessPixelsInCSharp
+{
+    public partial class Form1 : Form
+    {
+        private string _path;
+        private string _data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                _path = open.FileName;
+                textBox1.Text = _path;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _path = textBox1.Text;
+        }
+
+        private void GetData_Click(object sender, EventArgs e)
+        {
+            //Task<string> task = new Task<string>(CalculateAllPixels);
+            //task.Start();
+            //this.Text = "Processing...";
+            //textBox2.Text = await task;
+            //this.Text = "Form1";
+            
+            Thread thread = new Thread(() =>
+            {
+                _data = CalculateAllPixels();
+                Action action = () => textBox2.Text = _data;
+                BeginInvoke(action);
+            });
+            thread.Start();
+        }
+
+        private string CalculateAllPixels()
+        {
+            Bitmap bitmap = new Bitmap(Image.FromFile(_path));
+            var result = "";
+
+            for (var y = 0; y < bitmap.Height; y++)
+            {
+                var row = "";
+                for (var x = 0; x < bitmap.Width; x++)
+                {
+                    var onePixel = colorToString(bitmap.GetPixel(x, y));
+                    row += $"{onePixel} \t";
+                }
+                result += $"{row} {Environment.NewLine}";
+            }
+            return result;
+        }
+
+        private string colorToString(Color color)
+        {
+            return $"{color.R} {color.G} {color.B} {color.A}";
+        }
+        
+
+    }
+}
